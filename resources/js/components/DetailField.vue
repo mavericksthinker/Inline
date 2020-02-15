c75a838ae14f5e4c70f18195397c078b3475aeae
Mavericks(Bishal) <mavericksthinker@gmail.com>
2020-02-15 13:44:28 +0700

    Fixed minor css based on nova

--- resources/js/components/DetailField.vue
+++ resources/js/components/DetailField.vue
@@ -1,11 +1,12 @@
 <template>
-    <panel-item  :field="field">
+    <panel-item :field="field">
         <template v-if="field.inlineDetail">
             <p slot="value" class="text-90">
                 <label>
                     <input
                         type="text"
-                        class="form-input inline-field pl-0 w-full whitespace-no-wrap"
+                        class="form-control form-input pl-1 whitespace-no-wrap w-auto"
+                        :class="`text-${field.textAlign}`"
                         :placeholder="field.name"
                         v-model="value"
                         @blur="submit"
@@ -19,13 +20,15 @@
             <p slot="value" class="text-90">
                 <span
                    v-if="field.colorMap"
-                   class="whitespace-no-wrap px-2 py-1 rounded-full text-xs font-bold"
+                   class="whitespace-no-wrap rounded-full text-xs font-bold"
                    :style="{ backgroundColor: backgroundColor(), color: textColor() }"
+                   :class="`text-${field.textAlign}`"
                    v-html="field.value">
                 </span>
                 <span
                     v-else
-                    class="whitespace-no-wrap text-center"
+                    class="whitespace-no-wrap"
+                    :class="`text-${field.textAlign}`"
                     v-html="field.value">
                 </span>
             </p>
